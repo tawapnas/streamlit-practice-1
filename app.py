23d2d9a83bb6c765cf6ce0546d3157f320266af4
sanpawat <koyong555@hotmail.com>
2026-01-31 16:20:49 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+st.title("HBD Daddy!👋")
+st.markdown(
+    """ 
+
+
+    I'm dropping by to wish you the :rainbow[best] birthday ever - have an :rainbow[amazing] day!
+    Hope you have a :rainbow[fantastic] time ahead filled with joy and all the things that make you smile!
+
+    
+    Click the button below to gift yourself with some balloons! 🎈🎈🎈
+    """
+)
+if st.button("Send balloons!"):
+    st.balloons()
+st.image("aa.png")
+st.markdown(
+    """
+Click the button below for a special birthday surprise! 🎉"""
+    
+)
+
+if st.button("Surprise!"):
+    st.video("https://www.youtube.com/watch?v=UPhLKZu0how")
+
+    st.markdown(
+        """
+
+        
+        Before I go, Happy Birthday once again! 🎂🎉
+        -Neh
+        """
+    )
